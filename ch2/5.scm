9e0ca6e7f91f8c249fcbbf43470330daeb622fcd
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-27 15:39:45 +0530

    Solved 2.10 - Implement extend-env* for a-list environments

--- ch2/5.scm
+++ ch2/5.scm
@@ -9,3 +9,9 @@
 
 (define (has-binding? env var)
   (if (assoc var env) #t #f))
+
+
+(define (extend-env* vars vals env)
+  (if (not (= (length vars) (length vals)))
+      (error "vars and vals should be of the same lengh" vars vals)
+      (append (map cons vars vals) env)))
