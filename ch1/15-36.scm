5ec2ed0120fb22452df36e1f6fcc9d354c074cff
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-27 09:10:49 +0530

    Solved 1.16 - Implement invert

--- ch1/15-36.scm
+++ ch1/15-36.scm
@@ -4,3 +4,13 @@
   (if (zero? n)
       '()
       (cons x (duple (- n 1) x))))
+
+
+; invert: ListOf(List) -> ListOf(List)
+; Usage: invert ((x1 x2) (y1 y2) ...) = ((x2 x1) (y2 y1) ...)
+(define (invert lst)
+  (if (null? lst)
+      '()
+      (let ((list2 (car lst)))
+       (cons (list (cadr list2) (car list2))
+             (invert (cdr lst))))))
