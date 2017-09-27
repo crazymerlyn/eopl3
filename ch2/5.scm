f9407aa705258f4636420185ccae49dbf0f3d729
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-27 15:26:24 +0530

    Solved 2.5 - Implement an assoc list based env

---
+++ ch2/5.scm
@@ -0,0 +1,6 @@
+(define (empty-env) '())
+(define (extend-env var val env)
+  (cons (cons var val) env))
+(define (apply-env env var)
+  (cond ((assoc var env) => cdr)
+        (else (error "No binding for" var))))
