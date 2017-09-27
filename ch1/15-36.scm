c0a9cc09cc7694900379f7bf452f9cd2084d3eab
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-27 09:20:35 +0530

    Solved 1.17 and 1.18 - Implement down and swapper

--- ch1/15-36.scm
+++ ch1/15-36.scm
@@ -14,3 +14,23 @@
       (let ((list2 (car lst)))
        (cons (list (cadr list2) (car list2))
              (invert (cdr lst))))))
+
+
+; down: List -> ListOf(List)
+; Usage: (down (x1 x2 x3 ...)) = ((x1) (x2) (x3) ...)
+(define (down lst)
+  (map list lst))
+
+
+; swapper: Symbol x Symbol x S-List -> S-List
+; Usage: (swapper s1 s2 slist) = slist with all instances of s1 replaced
+;        with s2 and vice versa
+(define (swapper s1 s2 slist)
+  (define (swapper-exp s1 s2 sexp)
+    (if (symbol? sexp)
+        (cond ((eq? sexp s1) s2)
+              ((eq? sexp s2) s1)
+              (else sexp))
+        (swapper s1 s2 sexp)))
+  (map (lambda (sexp) (swapper-exp s1 s2 sexp))
+       slist))
