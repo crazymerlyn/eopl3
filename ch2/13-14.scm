b3328d9b6f7bbb7af3047d09ea1f44de21ee5817
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-27 16:01:52 +0530

    Solved 2.14 - Implement has-binding? for procedural environment

--- ch2/13-14.scm
+++ ch2/13-14.scm
@@ -1,14 +1,20 @@
 (define (empty-env)
   (list #t
         (lambda (var)
-          (error "No binding for" var))))
+          (error "No binding for" var))
+        (lambda (var) #f)))
 
 (define (extend-env var val env)
   (list #f
         (lambda (search-var)
           (if (eq? search-var var)
               val
-              ((cadr env) search-var)))))
+              ((cadr env) search-var)))
+        (lambda (search-var)
+          (if (eq? search-var var)
+              #t
+              ((caddr env) search-var)))))
 
 (define (empty-env? env) (car env))
 (define (apply-env env var) ((cadr env) var))
+(define (has-binding? env var) ((caddr env) var))
